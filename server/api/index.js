5b64d9801d5d9e62d78140d0606f35a2a547825e
Prakash Kumawat <prakashkumawat12245@gmail.com>
2026-08-24 10:20:33 +0530

    feat: bootstrap live database on first API request

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,11 @@
 import app from '../src/app.js';
 import {connectDB} from '../src/config/db.js';
+import {bootstrapDatabase} from '../src/utils/bootstrap.js';
 
 export default async function handler(req,res){
   try{
     await connectDB();
+    await bootstrapDatabase();
     return app(req,res);
   }catch(error){
     console.error('API startup error:',error.message);
