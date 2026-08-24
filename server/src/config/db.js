9b59743ec8dc6d73b0d33bd0a5971b1961dbde7c
Prakash Kumawat <prakashkumawat12245@gmail.com>
2026-08-24 10:21:56 +0530

    chore: support Vercel MongoDB Atlas environment variable

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,9 +4,10 @@ let connectionPromise;
 
 export async function connectDB(){
   if(mongoose.connection.readyState===1)return mongoose.connection;
-  if(!process.env.MONGO_URI)throw new Error('MONGO_URI is missing');
+  const uri=process.env.MONGO_URI||process.env.MONGODB_URI;
+  if(!uri)throw new Error('MongoDB connection string is missing');
   if(!connectionPromise){
-    connectionPromise=mongoose.connect(process.env.MONGO_URI,{serverSelectionTimeoutMS:10000})
+    connectionPromise=mongoose.connect(uri,{serverSelectionTimeoutMS:10000})
       .then(m=>{console.log('MongoDB connected');return m.connection})
       .catch(err=>{connectionPromise=null;throw err});
   }
