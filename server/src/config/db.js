b26bde971d775df4daa30728285edcfa8033df1a
Prakash Kumawat <prakashkumawat12245@gmail.com>
2026-08-24 10:19:25 +0530

    feat: make MongoDB connection serverless-safe

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,2 +1,14 @@
 import mongoose from 'mongoose';
-export async function connectDB(){if(!process.env.MONGO_URI)throw new Error('MONGO_URI is missing');await mongoose.connect(process.env.MONGO_URI);console.log('MongoDB connected')}
+
+let connectionPromise;
+
+export async function connectDB(){
+  if(mongoose.connection.readyState===1)return mongoose.connection;
+  if(!process.env.MONGO_URI)throw new Error('MONGO_URI is missing');
+  if(!connectionPromise){
+    connectionPromise=mongoose.connect(process.env.MONGO_URI,{serverSelectionTimeoutMS:10000})
+      .then(m=>{console.log('MongoDB connected');return m.connection})
+      .catch(err=>{connectionPromise=null;throw err});
+  }
+  return connectionPromise;
+}
