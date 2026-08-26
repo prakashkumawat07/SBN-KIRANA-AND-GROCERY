fb6c20d0f3474c2a7d7c6654f405c41d47116eb1
Prakash Kumawat <prakashkumawat12245@gmail.com>
2026-08-26 10:25:14 +0530

    feat: add admin marketing offer routes

--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -6,6 +6,7 @@ import {
   workers,createWorker,updateWorker,deleteWorker,cashEntries,createCashEntry,deleteCashEntry,
   admins,createAdmin,updateAdmin
 } from '../controllers/adminController.js';
+import {offers,createOffer,updateOffer,deleteOffer} from '../controllers/adminMarketingController.js';
 
 const r=Router();
 r.use(protect,adminOnly);
@@ -16,6 +17,7 @@ r.get('/customers',customers);r.get('/messages',messages);
 r.get('/paylater',payLaterCustomers);r.patch('/paylater/:id',updatePayLater);r.post('/paylater/:id/payment',recordPayLaterPayment);r.post('/paylater/:id/recovery',recordRecoveryAction);
 r.get('/stock',stock);r.patch('/stock/:id',updateStock);
 r.get('/reports',reports);
+r.get('/offers',offers);r.post('/offers',createOffer);r.patch('/offers/:id',updateOffer);r.delete('/offers/:id',deleteOffer);
 r.get('/workers',workers);r.post('/workers',createWorker);r.patch('/workers/:id',updateWorker);r.delete('/workers/:id',deleteWorker);
 r.get('/cash',cashEntries);r.post('/cash',createCashEntry);r.delete('/cash/:id',deleteCashEntry);
 r.get('/admins',admins);r.post('/admins',createAdmin);r.patch('/admins/:id',updateAdmin);
