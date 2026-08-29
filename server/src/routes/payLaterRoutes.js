2ac53224fee39b9ca9a465264ddc7c2bb9758df9
Prakash Kumawat <prakashkumawat12245@gmail.com>
2026-08-29 09:37:33 +0530

    feat: add PayLater KYC customer routes

--- server/src/routes/payLaterRoutes.js
+++ server/src/routes/payLaterRoutes.js
@@ -1,9 +1,12 @@
 import {Router} from 'express';
 import {protect} from '../middleware/auth.js';
 import {getMyPayLater,requestPayLater} from '../controllers/payLaterController.js';
+import {myPayLaterApplication,submitPayLaterApplication} from '../controllers/payLaterKycController.js';
 
 const r=Router();
 r.use(protect);
 r.get('/',getMyPayLater);
+r.get('/application',myPayLaterApplication);
+r.post('/application',submitPayLaterApplication);
 r.post('/request',requestPayLater);
 export default r;
