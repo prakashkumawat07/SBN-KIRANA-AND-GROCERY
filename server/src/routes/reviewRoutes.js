2d81aa23a9a7bb95b69b9cca86f6c434819d7461
Prakash Kumawat <prakashkumawat12245@gmail.com>
2026-08-29 10:24:59 +0530

    feat: expose review opportunity notifications

--- server/src/routes/reviewRoutes.js
+++ server/src/routes/reviewRoutes.js
@@ -1,7 +1,8 @@
 import {Router} from 'express';
 import {protect} from '../middleware/auth.js';
-import {productReviews,reviewEligibility,upsertReview} from '../controllers/reviewController.js';
+import {productReviews,reviewEligibility,reviewOpportunities,upsertReview} from '../controllers/reviewController.js';
 const r=Router();
+r.get('/opportunities',protect,reviewOpportunities);
 r.get('/:productId/eligibility',protect,reviewEligibility);
 r.get('/:productId',productReviews);
 r.post('/:productId',protect,upsertReview);
