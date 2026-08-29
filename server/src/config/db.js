73deb10f31e36e56e0b55a4f7e3847b82b1d520a
Prakash Kumawat <prakashkumawat12245@gmail.com>
2026-08-29 10:02:09 +0530

    security: keep required MongoDB security indexes active

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -11,7 +11,7 @@ export async function connectDB(){
   if(!uri)throw new Error('MongoDB connection string is missing');
   if(!/^mongodb(\+srv)?:\/\//i.test(uri))throw new Error('MongoDB connection string is invalid');
   if(!connectionPromise){
-    connectionPromise=mongoose.connect(uri,{serverSelectionTimeoutMS:10000,maxPoolSize:10,minPoolSize:0,autoIndex:process.env.NODE_ENV!=='production'})
+    connectionPromise=mongoose.connect(uri,{serverSelectionTimeoutMS:10000,maxPoolSize:10,minPoolSize:0})
       .then(m=>{if(process.env.NODE_ENV!=='production')console.log('MongoDB connected');return m.connection})
       .catch(err=>{connectionPromise=null;throw err});
   }
