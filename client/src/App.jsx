9ea712b57b3893eeea48a8c2b901d6b09712a28d
Prakash Kumawat <prakashkumawat12245@gmail.com>
2026-08-29 08:43:15 +0530

    feat: add customer bulk order route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes,Route} from 'react-router-dom';
 import './marketplace.css';
 import './recovery-policy.css';
 import './growth.css';
+import './bulk-orders.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -12,6 +13,7 @@ import Wishlist from './pages/Wishlist';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import Orders from './pages/Orders';
+import BulkOrders from './pages/BulkOrders';
 import Account from './pages/Account';
 import Contact from './pages/Contact';
 import Login from './pages/Login';
@@ -19,4 +21,4 @@ import Register from './pages/Register';
 import PayLater from './pages/PayLater';
 import InfoPage from './pages/InfoPage';
 
-export default function App(){return <><Navbar/><Routes><Route path="/" element={<Home/>}/><Route path="/products" element={<Products/>}/><Route path="/product/:id" element={<ProductDetail/>}/><Route path="/wishlist" element={<Wishlist/>}/><Route path="/cart" element={<Cart/>}/><Route path="/checkout" element={<ProtectedRoute><Checkout/></ProtectedRoute>}/><Route path="/account" element={<ProtectedRoute><Account/></ProtectedRoute>}/><Route path="/orders" element={<ProtectedRoute><Orders/></ProtectedRoute>}/><Route path="/paylater" element={<ProtectedRoute><PayLater/></ProtectedRoute>}/><Route path="/contact" element={<Contact/>}/><Route path="/info/:slug" element={<InfoPage/>}/><Route path="/login" element={<Login/>}/><Route path="/register" element={<Register/>}/></Routes><Footer/></>}
+export default function App(){return <><Navbar/><Routes><Route path="/" element={<Home/>}/><Route path="/products" element={<Products/>}/><Route path="/product/:id" element={<ProductDetail/>}/><Route path="/wishlist" element={<Wishlist/>}/><Route path="/cart" element={<Cart/>}/><Route path="/checkout" element={<ProtectedRoute><Checkout/></ProtectedRoute>}/><Route path="/account" element={<ProtectedRoute><Account/></ProtectedRoute>}/><Route path="/orders" element={<ProtectedRoute><Orders/></ProtectedRoute>}/><Route path="/bulk-orders" element={<ProtectedRoute><BulkOrders/></ProtectedRoute>}/><Route path="/paylater" element={<ProtectedRoute><PayLater/></ProtectedRoute>}/><Route path="/contact" element={<Contact/>}/><Route path="/info/:slug" element={<InfoPage/>}/><Route path="/login" element={<Login/>}/><Route path="/register" element={<Register/>}/></Routes><Footer/></>}
